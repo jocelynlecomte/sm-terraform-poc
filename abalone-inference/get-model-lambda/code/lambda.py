9b395cf4d9d65c1ace04b9a998cbf03abb57b989
Jocelyn Lecomte <dev@lecomte.me>
2025-07-01 08:45:41 +0200

    get model lambda creation

---
+++ abalone-inference/get-model-lambda/code/lambda.py
@@ -0,0 +1,69 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger()
+sm_client = boto3.client('sagemaker')
+
+def handler(event, context):
+    """
+    Lambda function to get a model URL from the registry.
+    """
+    logger.info("Received event: %s", event)
+
+    model_package_group_name = event.get('model_package_group_name')
+    if not model_package_group_name:
+        raise Exception("Model Package Group name is required in the event.")
+
+    latest_approved_package_arn =  get_latest_approved_package_arn(model_package_group_name)
+
+    model_package_description = sm_client.describe_model_package(ModelPackageName=latest_approved_package_arn)
+
+    logger.info(f"Model package description: {model_package_description}")
+
+    model_data_url = model_package_description["InferenceSpecification"]["Containers"][0]["ModelDataUrl"]
+
+    return {
+        "model_data_url": model_data_url,
+    }
+
+
+def get_latest_approved_package_arn(model_package_group_name):
+    """Gets the ARN of the latest approved model package for a model package group.
+    Args:
+        model_package_group_name: The model package group name.
+    Returns:
+        The SageMaker Model Package ARN.
+    """
+    try:
+        # Get the latest approved model package
+        # cf. https://docs.aws.amazon.com/sagemaker/latest/APIReference/API_ListModelPackages.html
+        try:
+            response = sm_client.list_model_packages(
+                ModelPackageGroupName=model_package_group_name,
+                ModelApprovalStatus="Approved",
+                SortBy="CreationTime",
+                MaxResults=100,
+            )
+        except ClientError as e:
+            error_msg = f"list_model_packages failed: {e.response['Error']['Code']}, {e.response['Error']['Message']}"
+            raise Exception(error_msg)
+
+        approved_packages = response["ModelPackageSummaryList"]
+
+        # Return error if no packages found
+        if len(approved_packages) == 0:
+            error_message = (
+                f"No approved ModelPackage found for ModelPackageGroup: {model_package_group_name}"
+            )
+            logger.error(error_message)
+            raise Exception(error_message)
+
+        # Return the pmodel package arn
+        model_package_arn = approved_packages[0]["ModelPackageArn"]
+        logger.info(f"Identified the latest approved model package ARN: {model_package_arn}")
+        return model_package_arn
+    except ClientError as e:
+        error_message = e.response["Error"]["Message"]
+        logger.error(error_message)
+        raise Exception(error_message)
---
+++ iac/get-model-lambda.tf
@@ -0,0 +1,69 @@
+locals {
+  get_model_lambda_name = "get_model_lambda"
+}
+
+# IAM role for Lambda execution
+data "aws_iam_policy_document" "get_model_lambda_assume_role_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "get_model_lambda_custom_policy" {
+  role       = aws_iam_role.get_model_lambda_role.name
+  policy_arn = aws_iam_policy.get_model_lambda_custom_policy.arn
+}
+
+resource "aws_iam_policy" "get_model_lambda_custom_policy" {
+  name        = "get_model_lambda_custom_policy"
+  policy      = data.aws_iam_policy_document.get_model_lambda_custom_policy_document.json
+}
+
+data "aws_iam_policy_document" "get_model_lambda_custom_policy_document" {
+  statement {
+    sid = "AllowS3Access"
+    actions = [
+      "sagemaker:ListModelPackages",
+      "sagemaker:DescribeModelPackage",
+    ]
+    resources = [
+      "arn:aws:sagemaker:us-east-1:557690605188:model-package/*",
+    ]
+  }
+}
+
+resource "aws_iam_role" "get_model_lambda_role" {
+  name               = local.get_model_lambda_name
+  assume_role_policy = data.aws_iam_policy_document.get_model_lambda_assume_role_policy.json
+}
+
+resource "aws_cloudwatch_log_group" "get_lambda_log_group" {
+  name              = "/aws/lambda/${local.get_model_lambda_name}"
+  retention_in_days = 14
+}
+
+data "archive_file" "get_model_lambda_zip" {
+  type        = "zip"
+  source_dir = "../abalone-inference/get-model-lambda/code"
+  output_path = "../abalone-inference/get-model-lambda/lambda.zip"
+}
+
+resource "aws_lambda_function" "get_model_lambda" {
+  function_name = "get-model-lambda"
+  role          = aws_iam_role.get_model_lambda_role.arn
+  runtime       = "python3.12"
+  filename      = data.archive_file.get_model_lambda_zip.output_path
+  source_code_hash = data.archive_file.get_model_lambda_zip.output_base64sha256
+  handler       = "lambda.handler"
+
+  depends_on = [
+    aws_cloudwatch_log_group.get_lambda_log_group
+  ]
+}
